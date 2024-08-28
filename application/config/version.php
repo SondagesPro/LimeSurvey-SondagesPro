447ba748b52191d7270eb9c1d63e8d6130a2c41a
Denis Chenu <contact@sondages.pro>
2024-08-28 10:19:23 +0200

    New feature #19722: Unable to use ExpressionScript code when export by remote control

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.6.71 (SondagesPro)';
+$config['versionnumber'] = '5.6.72 (SondagesPro)';
 $config['dbversionnumber'] = 499;
 $config['buildnumber'] = '';
 $config['updatable'] = false;
