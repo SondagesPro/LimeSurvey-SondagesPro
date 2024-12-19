d1182afc1c7d853ac8326cb507ebd556df070d04
Denis Chenu <contact@sondages.pro>
2024-12-19 12:07:38 +0100

    Fixed issue #19899: Order of question when copying are bad

--- application/controllers/QuestionAdministrationController.php
+++ application/controllers/QuestionAdministrationController.php
@@ -1698,6 +1698,7 @@ class QuestionAdministrationController extends LSBaseController
                     $newQuestionPosition = 1;
                     break;
                 default: //all other cases means after question X (the value coming from frontend is already correct)
+                    Question::increaseAllOrderNumbersForGroup($questionGroupId, $questionPosition);
                     $newQuestionPosition = $questionPosition;
             }
             $copyQuestionValues->setQuestionPositionInGroup($newQuestionPosition);
