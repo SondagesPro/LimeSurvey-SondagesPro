4278bbb03b0598af704c590039d471d220226742
Denis Chenu <contact@sondages.pro>
2025-06-26 18:34:56 +0200

    Release 5.6.80

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.6.79 (SondagesPro)';
+$config['versionnumber'] = '5.6.80 (SondagesPro)';
 $config['dbversionnumber'] = 499;
 $config['buildnumber'] = '';
 $config['updatable'] = false;
