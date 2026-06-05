64f37c008515c98b21bbca07431019bf2a64932a
Denis Chenu <denis@sondages.pro>
2026-06-05 16:39:01 +0200

    Fixed issue #20495: [security] Unrestricted getConfig() access in Twig sandbox

--- application/core/LS_Twig_Extension.php
+++ application/core/LS_Twig_Extension.php
@@ -649,9 +649,81 @@ class LS_Twig_Extension extends Twig_Extension
         return 'rgba(' . join(', ', $return) . ',' . $alpha . ')';
     }
 
+    /**
+     * Retrieve a configuration value restricted to an internal allowlist.
+     *
+     * Returns the configuration value for $name when $name is present in the built-in allowlist
+     * or in the `twig_getConfig_extraallowlist` configuration (when that extra list is an array).
+     * Returns `false` for any key that is not allowed.
+     *
+     * @param string $name The configuration key to read.
+     * @return mixed The configuration value when allowed, `false` otherwise.
+     */
     public static function getConfig($item)
     {
-        return Yii::app()->getConfig($item);
+        /* Core allowedlist */
+        $coreAllowedList = self::getAllowedConfig();
+        if (in_array($name, $coreAllowedList, true)) {
+            return App()->getConfig($name);
+        }
+        /* if allowlist is an array, use it */
+        $extraAllowedList = App()->getConfig('twig_getConfig_extraallowlist');
+        if (is_array($extraAllowedList) && in_array($name, $extraAllowedList, true)) {
+            return App()->getConfig($name);
+        }
+        return false;
+    }
+
+    /**
+     * Fixed allowlist of configuration keys considered safe for exposure to templates.
+     *
+     * The array contains the configuration key names that are permitted to be read
+     * by template-level configuration accessors.
+     *
+     * @return string[] Array of allowed configuration key names.
+     */
+    private static function getAllowedConfig()
+    {
+        return [
+            /* Used for global and error page */
+            'sitename',
+            'siteadminname',
+            'siteadminemail',
+            /* Needed by question view */
+            'surveyID',
+            /* Clearly public */
+            'defaultlang',
+            'defaulttheme',
+            'defaultfixedtheme',
+            'maintenancemode',
+            'repeatheadings',
+            'minrepeatheadings',
+            'printanswershonorsconditions',
+            /* Potential usage in theme */
+            'generalscripts', // Used in core Questions
+            'shownoanswer',
+            'showpopups',
+            'demoMode',
+            /* url */
+            'publicurl',
+            'tempurl',
+            'assets',
+            'imageurl',
+            'uploadurl',
+            'standardthemerooturl',
+            'adminscripts',
+            'generalscripts',
+            'styleurl',
+            'publicstyle',
+            'publicstyleurl',
+            'sCKEditorURL',
+            'userthemerooturl',
+            'adminimageurl',
+            'applicationurl',
+            'extensionsurl',
+            'adminstyleurl',
+            'userfontsurl',
+        ];
     }
 
 
